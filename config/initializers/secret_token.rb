8ec7b02d8a44d18e1de6336764aafde55844249e
Kit Hennessey <kit.hennessey@gmail.com>
2012-02-08 22:54:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdwNotes::Application.config.secret_token = '7eaa9b47a5bb9f261bd1b7a6e228eda49bed2cdfcad4c96e955a39fdd0807b99810d85fff0519443d61ef0ba5957f4832ed03c6093c1f08bc6fa54ee766cb2d9'
